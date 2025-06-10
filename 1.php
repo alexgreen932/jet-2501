a0340d251847071290146ce42eb4e96cba648b67
alexgreen932 <alexgreen932@gmail.com>
2025-06-10 16:23:41 +0300

    pages done

--- 1.php
+++ 1.php
@@ -1,69 +1,7 @@
-i missed a table, can you change it similar to below, same classes simple code, to save me time
+i'm not sure you can do it, but if you can, please do the this 
 
-<div class="table-wrapper">
-    <table>
-        <thead>
-            <tr>
-                <th>Promotion</th>
-                <th>Details</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Regular Player Bonus</td>
-                <td>340% bonus plus 30 free spins, three times daily for deposits of at least $35.</td>
-            </tr>
-            <!-- etc -->
-        </tbody>
-    </table>
-</div>
+1. here logo, it's a bit corrupt at top can you complete two 3d '2'
+2. remove background, so output is png with transparent background  
+3. keep one image size as iz, 2nd height 60px 
 
-//source code
-
-<figure class="table" style="float: left; width: 100%;">
-    <div class="table-responsive-sm">
-        <table class="table table-sm table-striped table-bordered table-hover">
-            <colgroup>
-                <col style="width: 50%;">
-                <col style="width: 50%;">
-            </colgroup>
-            <thead>
-                <tr>
-                    <th style="border: 1pt solid rgb(0, 0, 0); padding: 5pt; vertical-align: top;">Pros</th>
-                    <th style="border: 1pt solid rgb(0, 0, 0); padding: 5pt; vertical-align: top;">Cons</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td style="border: 1pt solid rgb(0, 0, 0); padding: 5pt; vertical-align: top;">Daily promotions and
-                        significant bonuses enhance the gaming experience.</td>
-                    <td style="border: 1pt solid rgb(0, 0, 0); padding: 5pt; vertical-align: top;">Limited variety in
-                        game types, with no live dealer or crash games available.</td>
-                </tr>
-                <tr>
-                    <td style="border: 1pt solid rgb(0, 0, 0); padding: 5pt; vertical-align: top;">Continuous addition
-                        of new games keeps the library fresh and exciting.</td>
-                    <td style="border: 1pt solid rgb(0, 0, 0); padding: 5pt; vertical-align: top;">No mobile app
-                        available for download.</td>
-                </tr>
-                <tr>
-                    <td style="border: 1pt solid rgb(0, 0, 0); padding: 5pt; vertical-align: top;">Customer support is
-                        available 24/7 via live chat and email.</td>
-                    <td style="border: 1pt solid rgb(0, 0, 0); padding: 5pt; vertical-align: top;">&nbsp;</td>
-                </tr>
-                <tr>
-                    <td style="border: 1pt solid rgb(0, 0, 0); padding: 5pt; vertical-align: top;">The A Big Candy
-                        Casino is designed for easy navigation.</td>
-                    <td style="border: 1pt solid rgb(0, 0, 0); padding: 5pt; vertical-align: top;">&nbsp;</td>
-                </tr>
-                <tr>
-                    <td style="border: 1pt solid rgb(0, 0, 0); padding: 5pt; vertical-align: top;">Fully compatible with
-                        mobile devices for gaming on the go.</td>
-                    <td style="border: 1pt solid rgb(0, 0, 0); padding: 5pt; vertical-align: top;">&nbsp;</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-</figure>
-
-thanks
+just i have no installed photoshop on my linux, and installation via wine will take a lot of time
