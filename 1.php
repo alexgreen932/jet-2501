0160da17cf635ba522c67315e96ec0c6ae66862c
alexgreen932 <alexgreen932@gmail.com>
2025-06-11 13:20:10 +0300

    Final

--- 1.php
+++ 1.php
@@ -1,65 +1,110 @@
+one small thing 
 
-footer logo is another
-i have two ones this at top
-<img src="http://dev.site/wp-content/themes/jet-2501/img/logo.png" alt="Site Logo">
-see rendered code
-though actually it show correct code
-<header id="jet-header" class="jet-header">
-		<div class="container">
-			
-<a href="http://dev.site" class="site-logo"> <img src="http://dev.site/wp-content/themes/jet-2501/img/logo.png" alt="Site Logo" />  </a>	</div>
-	</header>
-i guess any js code adds it
-will try to remove all scrips now
+in FAQ boxes heading has a gap(if heading is in two lines only), examination show no any padding or marin, how to fix it?
+see screenshot 
 
-switched off all scripts same
+current faq scss 
+// Variables
+$color-text: #fff;
+$color-question: #eca21a;
 
-//above in page source cane, but here in dev console
+$faq-bg: $ac-darken;
+$faq-hover: #1e2f22;
+
+$color-answer: #aaa;
+
+.f-faq {
+    // padding: 2rem 1rem;
+    // max-width: 1200px;
+    margin: auto;
+
+    .f-title {
+        font-size: 2rem;
+        font-weight: bold;
+        margin-bottom: 1.5rem;
+        color: $color-text;
+    }
+
+    .f-list {
+        display: grid;
+        grid-template-columns: 1fr;
+        gap: 1rem;
+        list-style: none;
+        padding: 0;
+
+        @media (min-width: 768px) {
+            grid-template-columns: 1fr 1fr;
+        }
+    }
+
+    .f-item {
+        background: $faq-bg;
+        border-radius: 0.5rem;
+        overflow: hidden;
+        transition: background 0.3s ease;
+
+        &:hover {
+            background: $faq-hover;
+            color: #fff;
+        }
+
+        .f-question {
+            background-color: inherit;
+            // all: unset;
+            border-image-width: 0;
+            border-width: 0;
+            text-align-last: left;
+            display: block;
+            width: 100%;
+            padding: 1rem;
+            font-size: 1rem;
+            font-weight: 700;
+            color: $color-question;
+            text-transform: none;
+            cursor: pointer;
+            // transition: background 0.3s ease;
+            position: relative;
+            padding-right: 30px;
+            &:hover{
+                background: transparent!important;
+            }
+
+            &::after {
+                position: absolute;
+                right: 10px;
+                top: 10px;
+                content: '+';
+                font-weight: 700;
+                font-size: 1.5rem;
+                // float: right;
+                // transition: transform 0.3s;
+            }
+
+            &.open::after {
+                content: '×';
+                transform: rotate(180deg);
+            }
 
 
-<body class="home wp-singular page-template-default page page-id-7 logged-in wp-theme-jet-2501" inmaintabuse="1"><a href="http://dev.site" class="site-logo"> <img src="http://dev.site/wp-content/themes/jet-2501/img/logo.png" alt="Site Logo">  </a>
+        }
 
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+        .f-answer {
+            max-height: 0;
+            opacity: 0;
+            overflow: hidden;
+            transition: all 0.3s ease;
+            padding: 0 1rem;
 
-	<title>Blank</title>
-<meta name="robots" content="max-image-preview:large">
-	<style>img:is([sizes="auto" i], [sizes^="auto," i]) { contain-intrinsic-size: 3000px 1500px }</style>
-	<link rel="alternate" type="application/rss+xml" title="Blank » Feed" href="http://dev.site/feed/">
-<link rel="alternate" type="application/rss+xml" title="Blank » Comments Feed" href="http://dev.site/comments/feed/">
+            &.open {
+                max-height: 300px;
+                opacity: 1;
+                padding-bottom: 1rem;
+            }
 
-<link rel="stylesheet" id="wp-block-library-css" href="http://dev.site/wp-includes/css/dist/block-library/style.min.css?ver=6.8.1" media="all">
-//.... 
-<link rel="stylesheet" id="jet-style-css" href="http://dev.site/wp-content/themes/jet-2501/style.css?ver=1.0.0" media="all">
-<link rel="stylesheet" id="theme-css" href="http://dev.site/wp-content/themes/jet-2501/css/style.css?ver=1.0.0" media="all">
-<link rel="https://api.w.org/" href="http://dev.site/wp-json/"><link rel="alternate" title="JSON" type="application/json" href="http://dev.site/wp-json/wp/v2/pages/7"><link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://dev.site/xmlrpc.php?rsd">
-<meta name="generator" content="WordPress 6.8.1">
-<link rel="canonical" href="http://dev.site/">
-<link rel="shortlink" href="http://dev.site/">
-<link rel="alternate" title="oEmbed (JSON)" type="application/json+oembed" href="http://dev.site/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fdev.site%2F">
-<link rel="alternate" title="oEmbed (XML)" type="text/xml+oembed" href="http://dev.site/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fdev.site%2F&amp;format=xml">
-	<!-- //TODO DEV PHP LIVE RELOAD WATCHER - REMOVE ON PROD -->
-	<script>
-		setInterval(() => {
-			fetch('/reload.txt')
-				.then(r => r.text())
-				.then(time => {
-					if (!window.lastReloadTime) window.lastReloadTime = time;
-					if (window.lastReloadTime != time) location.reload();
-				});
-		}, 1000);
-	</script>
-
-
-
-
-		<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#primary">Skip to content</a>
-
-		<header id="jet-header" class="jet-header">
-		<div class="container">
-			
-<a href="http://dev.site" class="site-logo"> <img src="http://dev.site/wp-content/themes/jet-2501/img/logo.png" alt="Site Logo">  </a>	</div>
-	</header><main id="primary" class="container">
-	//rest of code....
+            p {
+                margin: 1rem 0 0;
+                color: $color-answer;
+            }
+        }
+    }
+}
